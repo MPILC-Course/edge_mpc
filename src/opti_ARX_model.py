62e32f342939bd3b2476d7392712a3595b9564d7
Stepzi <s.storz01@gmail.com>
2024-02-20 18:48:01 +0100

    speed comparison

--- src/opti_ARX_model.py
+++ src/opti_ARX_model.py
@@ -9,8 +9,12 @@ class ARXModel:
         self.u_k = u0;
 
     def update_state(self,u):
-        y = self.A @ self.y_k +self.B @ u + self.C
+        y = self.A @ self.y_k +self.B @ self.u_k + self.C
+        
         y_k_old = self.y_k[:-self.A.shape[0]] # Remove the oldest ny elements
         self.y_k = ca.vcat([y,y_k_old])
+
+        u_k_old = self.u_k[:-u.shape[0]] # Remove the oldest nu elements
+        self.u_k = ca.vcat([u,u_k_old])
 
         return y
