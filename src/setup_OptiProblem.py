38423a1fb9df16a6e1a33ebb8ed839ce5f490cdc
Stepzi <s.storz01@gmail.com>
2024-04-18 14:58:54 +0200

    11h comparison run

--- src/setup_OptiProblem.py
+++ src/setup_OptiProblem.py
@@ -1,6 +1,41 @@
 import casadi as ca
 import numpy as np
 
+def system_dynamics(w,Qout,Qin_est,P,E,h,Ts):
+    #============= Tank Model ==================
+    A = 18
+
+    #=========== Parameters of the static models
+    B1_pressure = np.array([9.2727E-05])
+    B2_pressure = np.array([2.5979E-08])
+    C_pressure = np.array([5.3955E-01])
+
+    #=========== Parameters of the ARX model
+    # array of params
+    A_outflow = np.array([1.1493E-01, 3.5884E-07, -2.9890E-07, -8.4661E-02, 7.5640E-04,
+                        -3.8695E-04, 3.9287E-04, -6.8566E-07, -1.5757E-04, 1.0592E-03,
+                        2.7095E-07, -4.1301E-01, 7.0157E-05]).reshape((1,-1))
+
+    A_power = np.hstack([np.diag([1.6108E-01, 3.6872E-03, 2.3590E-01]), np.diag([9.5030E-02, 3.1648E-03, 1.9355E-01])])
+
+    B_power = np.diag([3.5117E-02, 4.1216E-02, 2.7095E-02])
+
+    C_power = np.array([0, 0, 1.5144E-03]).reshape((3,-1))
+
+
+    Qout_next = ca.if_else(w[0,-1] <= 600, 3.2216 + 0.08378681 * w[0,-1],3.22 + 0.083 * 600 + 0.8371 * (w[0,-1] - 600),True)+ ca.if_else(w[1,-1] <= 600,3.2216 + 0.08378681 * w[1,-1],3.22 + 0.083 * 600 + 0.8371 * (w[1,-1] - 600),True)+ ca.if_else(w[2,-1] <= 600,3.2216 + 0.08378681 * w[2,-1],3.22 + 0.083 * 600 + 0.8371 * (w[2,-1] - 600),True)
+
+
+    E_next = A_power @ ca.vcat([E[:,-1],E[:,-2]]) + B_power @ w[:,-1] + C_power
+
+    P_next = B1_pressure @ Qout[-1] + B2_pressure @ Qout[-1]**2 + C_pressure
+    
+    h_next = h[-1] + Ts/3600*(Qin_est[-1]-Qout[-1])/A
+
+    return Qout_next,E_next,P_next,h_next
+
+
+
 def setup_OptiProblem(N,zs,Ts)->ca.Function:
     # Create an Opti instance
     opti = ca.Opti()
@@ -23,10 +58,10 @@ def setup_OptiProblem(N,zs,Ts)->ca.Function:
         }
     opti.solver("ipopt",p_opts,s_opts)
 
-    #============= Tank Model ==================
+    # #============= Tank Model ==================
     A = 18
 
-    #=========== Parameters of the static models
+    # #=========== Parameters of the static models
     B1_pressure = np.array([9.2727E-05])
     B2_pressure = np.array([2.5979E-08])
     C_pressure = np.array([5.3955E-01])
@@ -76,13 +111,21 @@ def setup_OptiProblem(N,zs,Ts)->ca.Function:
     objective = 0
 
     for t in range(0, N+zs): # having 0 is technically wrong, but it works and makes the code run 2x faster??
-        objective += 0.05*(E[:,t].T @ E[:,t]) + 500*(h[t]-h_ref[t])**2  + 0.05*((w[:,t]-w[:,t-1]).T @ (w[:,t]-w[:,t-1]))+1000*s_P[t]**2 + 1000*s_h[t]**2 + 0.5*(ca.if_else(trigger[0] > 0, 0,w[0,t]) + ca.if_else(trigger[1] > 0, 0,w[1,t]) +ca.if_else(trigger[2] > 0, 0,w[2,t]))
+        # objective += 0.05*(E[:,t].T @ E[:,t]) + 500*(h[t]-h_ref[t])**2  + 0.05*((w[:,t]-w[:,t-1]).T @ (w[:,t]-w[:,t-1]))+1000*s_P[t]**2 + 1000*s_h[t]**2 + 0.5*(ca.if_else(trigger[0] > 0, 0,w[0,t]) + ca.if_else(trigger[1] > 0, 0,w[1,t]) +ca.if_else(trigger[2] > 0, 0,w[2,t]))
+        objective += 0.05*(E[:,t].T @ E[:,t]) + 10*(h[t]-h_ref[t])**2  + 0.05*((w[:,t]-w[:,t-1]).T @ (w[:,t]-w[:,t-1]))+1000*s_P[t]**2 + 1000*s_h[t]**2 + 0.5*(ca.if_else(trigger[0] > 0, 0,w[0,t]) + ca.if_else(trigger[1] > 0, 0,w[1,t]) +ca.if_else(trigger[2] > 0, 0,w[2,t]))
 
     opti.minimize(objective)   
 
 
     # ARX model constraints
-    for t in range(zs, N+zs):   
+    for t in range(zs, N+zs):
+
+        # # Qout_next,E_next,P_next,h_next = system_dynamics(w[:,t-zs:],Qout[t-zs:],Qin_est[t-zs:],P[t-zs:],E[:,t-zs:],h[t-zs:],Ts)
+        # Qout_next,E_next,P_next,h_next = system_dynamics(w,Qout,Qin_est,P,E,h,Ts)
+        # opti.subject_to(Qout[t] == Qout_next)
+        # opti.subject_to(E[:,t] == E_next)
+        # opti.subject_to(P[t] == P_next)
+        # opti.subject_to(h[t] == h_next)
         opti.subject_to(Qout[t] ==ca.if_else( 
                                     w[0,t-1] <= 600,
                                     3.2216 + 0.08378681 * w[0,t-1],  # First segment equation
@@ -102,7 +145,7 @@ def setup_OptiProblem(N,zs,Ts)->ca.Function:
 
         opti.subject_to(E[:,t] == A_power @ ca.vcat([E[:,t-1],E[:,t-2]]) + B_power @ w[:,t-1] + C_power)
 
-        opti.subject_to(P[t] == B1_pressure @ Qout[t] + B2_pressure @ Qout[t]**2 + C_pressure) 
+        opti.subject_to(P[t] == B1_pressure @ Qout[t-1] + B2_pressure @ Qout[t-1]**2 + C_pressure) 
         
         opti.subject_to(h[t] == h[t-1] + Ts/3600*(Qin_est[t-1]-Qout[t-1])/A)
 
@@ -149,4 +192,5 @@ def setup_OptiProblem(N,zs,Ts)->ca.Function:
     inputs = [Qin_est,Qout_meas,h_meas,w_meas,E_meas,P_meas,trigger,h_ref]
     outputs = [w,Qout,h,E,P,effi]
     mpc_controller = opti.to_function("mpc_controller",inputs,outputs)
+    
     return mpc_controller
